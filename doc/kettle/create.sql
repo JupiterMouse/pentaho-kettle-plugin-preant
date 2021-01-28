6683fcb806008d3f4138894937325a9e83239d3d
JupiterMouse <986554261@qq.com>
2021-01-28 13:40:11 +0800

    :mono: 测试文件

---
+++ doc/kettle/create.sql
@@ -0,0 +1,15 @@
+create database kettle;
+use kettle;
+create table kettle_job_test
+(
+    id int null,
+    name varchar(128) null,
+    age int null
+);
+
+create table kettle_job_copy
+(
+    id int null,
+    name varchar(128) null,
+    age int null
+);
